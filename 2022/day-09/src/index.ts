418cb0a9f62a070419f20973277b2b8c188a1c7c
Jan Plazovnik <jan.plazovnik@gmail.com>
2022-12-09 15:03:25 +0100

    chore(2022): removed comments because they ugly

--- 2022/day-09/src/index.ts
+++ 2022/day-09/src/index.ts
@@ -2,7 +2,6 @@ import * as fs from "fs";
 import * as path from "path";
 import Point from "./Point";
 
-// Read the input file
 const lines = fs
     .readFileSync(path.join(__dirname, "/input/input.txt"), "utf8")
     .split("\n")
@@ -12,18 +11,13 @@ const lines = fs
     });
 
 function getTailVisits(length: number) {
-    // All knots on the rope start at (0, 0)
     const rope: Point[] = new Array(length).fill(0).map(() => new Point(0, 0));
     const visited: Point[] = [new Point(0, 0)];
 
-    // For each directional input, move the head and the tails
     for (const { dir, num } of lines) {
-        // Move the head and the tails in the given direction by 1 until we reach the given number
         for (let i = 0; i < num; i++) {
-            // Move the head
             rope[rope.length - 1].move(dir, 1);
 
-            // Move each tail except the last one towards the next tail
             for (let j = rope.length - 2; j >= 0; j--) {
                 if (rope[j].isAdjacentOrOverlapping(rope[j + 1])) {
                     continue;
@@ -31,7 +25,6 @@ function getTailVisits(length: number) {
 
                 rope[j].moveTowards(rope[j + 1]);
 
-                // Add the position to the list of visited positions, if it hasn't been visited yet
                 if (j === 0 && visited.every((p) => !p.equals(rope[j]))) {
                     visited.push(new Point(rope[j].x, rope[j].y));
                 }
@@ -42,5 +35,5 @@ function getTailVisits(length: number) {
     return visited.length;
 }
 
-console.log(getTailVisits(2));
-console.log(getTailVisits(10));
+console.log(getTailVisits(2)); // 6284
+console.log(getTailVisits(10)); // 2661

fbc0eb35920ae8fa7c93ffe788e3cb88050c348d
Jan Plazovnik <jan.plazovnik@gmail.com>
2022-12-13 18:04:19 +0100

    chore(2022): removed InputType type from local

--- 2022/src/day-12/types.ts
+++ 2022/src/day-12/types.ts
@@ -1,10 +1,5 @@
 export type CoordinatePair = [number, number];
 
-export enum InputType {
-    Part1,
-    Part2,
-}
-
 export interface StartPoint {
     point: CoordinatePair;
     steps: number;

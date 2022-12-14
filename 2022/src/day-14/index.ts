a9ddfe023815bfba79e1d81648df7008a74ae112
Jan Plazovnik <jan.plazovnik@gmail.com>
2022-12-14 21:11:41 +0100

    chore(2022): optimize day 14

--- 2022/src/day-14/index.ts
+++ 2022/src/day-14/index.ts
@@ -4,13 +4,15 @@ import { InputType, Timer } from '../common/utils';
 
 const input = fs.readFileSync(path.join(__dirname, '/input/input.txt'), 'utf8').trimEnd();
 
-function str(x: number, y: number): string {
-    return `${x},${y}`;
+function hash(x: number, y: number): number {
+    return x * 10000 + y;
 }
 
-function parseInput(input: string): Set<string> {
+let maxY = 0;
+
+function parseInput(input: string): Set<number> {
     const rocks = input.split('\n');
-    const rockPixels: Set<string> = new Set();
+    const rockPixels: Set<number> = new Set();
 
     // Get all the pixels where the rock has to be drawn using #
     for (const rock of rocks) {
@@ -21,20 +23,23 @@ function parseInput(input: string): Set<string> {
             const [x1, y1] = path[i].split(',').map(Number);
             const [x2, y2] = path[i + 1].split(',').map(Number);
 
+            // Get the max Y value, we'll use this later to stop the simulation when the sand overflows
+            maxY = Math.max(maxY, y1, y2);
+
             // Get the points between the two points, be mindful of the direction
             if (x1 === x2) {
                 const yStart = Math.min(y1, y2);
                 const yEnd = Math.max(y1, y2);
 
                 for (let y = yStart; y <= yEnd; y++) {
-                    rockPixels.add(str(x1, y));
+                    rockPixels.add(hash(x1, y));
                 }
             } else {
                 const xStart = Math.min(x1, x2);
                 const xEnd = Math.max(x1, x2);
 
                 for (let x = xStart; x <= xEnd; x++) {
-                    rockPixels.add(str(x, y1));
+                    rockPixels.add(hash(x, y1));
                 }
             }
         }
@@ -48,19 +53,16 @@ function parseInput(input: string): Set<string> {
 // The runtime was SLOW. Not just SLOW, but SLOW^2. So don't be me. I waited like 5 minutes for part 1, didn't even bother with part 2.
 // With a Set<string> the runtime is much better, we're talking seconds here.
 // Don't be me.
+// Edit: We're now back to Set<number> but now I'm hashing the values to get a unique number.
 function solve(input: string, type: InputType): number {
-    const origin = [500, 0];
     const rockPixels = parseInput(input);
-    const sandPixels: Set<string> = new Set();
-
-    // I could have just stored this when parsing the input, but it is what it is.
-    const maxY = Math.max(...Array.from(rockPixels.values()).map((value) => Number(value.split(',')[1])));
+    const sandPixels: Set<number> = new Set();
 
     // Keep spawning sand until it's overflowing
     let simulating = true;
     while (simulating) {
         // Create a new sand particle at the origin
-        const particle = [...origin];
+        const particle = [500, 0];
 
         // Keep dropping it until it stops or overflows
         let falling = true;
@@ -68,9 +70,9 @@ function solve(input: string, type: InputType): number {
             const [x, y] = particle;
 
             // Do all the checks here so i can merge part1 and 2 into one function and not have to copy paste the code
-            const down = !rockPixels.has(str(x, y + 1)) && !sandPixels.has(str(x, y + 1));
-            const left = !rockPixels.has(str(x - 1, y + 1)) && !sandPixels.has(str(x - 1, y + 1));
-            const right = !rockPixels.has(str(x + 1, y + 1)) && !sandPixels.has(str(x + 1, y + 1));
+            const down = !rockPixels.has(hash(x, y + 1)) && !sandPixels.has(hash(x, y + 1));
+            const left = !rockPixels.has(hash(x - 1, y + 1)) && !sandPixels.has(hash(x - 1, y + 1));
+            const right = !rockPixels.has(hash(x + 1, y + 1)) && !sandPixels.has(hash(x + 1, y + 1));
 
             // Check if the sand is overflowing
             if (type === InputType.Part1 && y > maxY) {
@@ -100,12 +102,12 @@ function solve(input: string, type: InputType): number {
             }
 
             // We're stuck, add the sand to the set
-            sandPixels.add(str(x, y));
+            sandPixels.add(hash(x, y));
             falling = false;
         }
 
         // We're back at the origin, stop simulating
-        if (type === InputType.Part2 && particle.toString() === origin.toString()) {
+        if (type === InputType.Part2 && particle[0] === 500 && particle[1] === 0) {
             simulating = false;
         }
     }
